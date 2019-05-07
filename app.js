6506739ae8f4354164f9a59c4e2e39d0b09497a6
Your Name <37543357+ArtemVlasov@users.noreply.github.com>
2019-05-08 00:17:21 +0300

    end2

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
 
 const minis =(name, model, army, owner,phone, image)=>({name, model, army, owner,phone, image});
-
+const log=(text, type, date = new Date())=>({text, type, date});
 
 const miniatures =[
-   minis ('Dread scythe Harrridans', 'nighthaunt','death', 'Max', '+380931234567',  'images/99120207067_DreadscytheHarrridans01.jpg'),
-    minis ('Bladegheist Revenants', 'nighthaunt','death', 'Alex', '+380931236785',  'images/99120207066_BladegheistRevenants01.jpg'),
-    minis ('Chainrasps', 'nighthaunt','death', 'Vlad', '+380931234987',  'images/99120207059_ETBChainrasps02.jpg')
+   minis ('Dread scythe ', 'nighthaunt','death', 'Max', '+380931234567',  'images/99120207067_DreadscytheHarrridans01.jpg'),
+    minis ('Revenants', 'noble','death', 'Alex', '+380931236785',  'images/99120207066_BladegheistRevenants01.jpg'),
+    minis ('Chainrasps', 'ghoust','death', 'Vlad', '+380931234987',  'images/99120207059_ETBChainrasps02.jpg')
 ];
 
 
 new Vue({
-el:'#app',
-    data:{
-        miniatures : miniatures,
+    el: '#app',
+    data: {
+        miniatures: miniatures,
         minis: miniatures[0],
+        logs: [],
         selectedMinisIndex: 0,
-        phoneVisibility: false
+        phoneVisibility: false,
+        search: '',
+        modalVisibility : false
     },
-    methods:{
-        selectCar: function(index){
-            console.log('click',index);
+    methods: {
+        selectCar(index) {
             this.minis = miniatures[index];
             this.selectedMinisIndex = index
+        },
+        newOrder(){
+            this.modalVisibility = false
+            this.logs.push(
+                log(`Success order: ${this.minis.name}- ${this.minis.model}`, 'ok')
+            )
+    },
+        cancelOrder(){
+            this.modalVisibility = false
+            this.logs.push(
+                log(`Cancelled order: ${this.minis.name}- ${this.minis.model}`, 'cnl')
+            )
+        }
+    },
+    computed: {
+        phoneBtnText() {
+            return this.phoneVisibility ? 'Hide Phone' : 'Show phone'
+        },
+        filtredMinis() {
+            return this.miniatures.filter(minis => {
+                return minis.name.indexOf(this.search) > -1 || minis.model.indexOf(this.search) > -1
+            })
+        }
+    },
+    filters:{
+        date(value){
+            return value.toLocaleString()
         }
     }
-});
+})
